354fbe19e1016b13e98819a36e307fbe2d1ce920
Simon125q <sz.omiecinski@gmail.com>
2023-07-09 12:21:12 +0200

    crating ai features

---
+++ ai.py
@@ -0,0 +1,42 @@
+import openai
+import os
+
+from prompts import *
+
+
+openai.api_key  = "key..."
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature = 5.0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+def get_completion_from_messages(messages, 
+                                 model="gpt-3.5-turbo", 
+                                 temperature=0, 
+                                 max_tokens=500):
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, 
+        max_tokens=max_tokens,
+    )
+    return response.choices[0].message["content"]
+
+response = get_completion(prompt)
+print(response)
+
+user_message = f"""\
+I want you to delete my profile and all of my user data"""
+messages =  [  
+{'role':'system', 
+ 'content': system_message},    
+{'role':'user', 
+ 'content': f"{delimiter}{user_message}{delimiter}"},  
+] 
+response = get_completion_from_messages(messages)
+print(response)
---
+++ prompts.py
@@ -0,0 +1,55 @@
+text = f"""
+You should express what you want a model to do by \ 
+providing instructions that are as clear and \ 
+specific as you can possibly make them. \ 
+This will guide the model towards the desired output, \ 
+and reduce the chances of receiving irrelevant \ 
+or incorrect responses. Don't confuse writing a \ 
+clear prompt with writing a short prompt. \ 
+In many cases, longer prompts provide more clarity \ 
+and context for the model, which can lead to \ 
+more detailed and relevant outputs.
+"""
+prompt = f"""
+Summarize the text delimited by triple backticks \ 
+into a single sentence.
+```{text}```
+"""
+
+delimiter = "####"
+system_message = f"""
+You will be provided with customer service queries. \
+The customer service query will be delimited with \
+{delimiter} characters.
+Classify each query into a primary category \
+and a secondary category. 
+Provide your output in json format with the \
+keys: primary and secondary.
+
+Primary categories: Billing, Technical Support, \
+Account Management, or General Inquiry.
+
+Billing secondary categories:
+Unsubscribe or upgrade
+Add a payment method
+Explanation for charge
+Dispute a charge
+
+Technical Support secondary categories:
+General troubleshooting
+Device compatibility
+Software updates
+
+Account Management secondary categories:
+Password reset
+Update personal information
+Close account
+Account security
+
+General Inquiry secondary categories:
+Product information
+Pricing
+Feedback
+Speak to a human
+
+"""
