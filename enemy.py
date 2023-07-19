34660240e4e16c90c97d5ea08e4bd9f2ddb16ff7
Simon125q <sz.omiecinski@gmail.com>
2023-07-19 21:11:26 +0200

    add enemy movement

--- enemy.py
+++ enemy.py
@@ -2,6 +2,7 @@ import pygame
 from settings import *
 from entity import Entity
 from support import *
+from debug import debug
 
 class Enemy(Entity):
     def __init__(self, monster_name, pos, groups, obstacle_sprites):
@@ -18,8 +19,61 @@ class Enemy(Entity):
         self.hitbox = self.rect.inflate(-4, -12)
         self.obstacle_sprites = obstacle_sprites
         
+        # stats
+        self.monster_name = monster_name
+        monster_info = monster_data[self.monster_name]
+        self.health = monster_info['health']
+        self.exp = monster_info['exp']
+        self.speed = monster_info['speed']
+        self.attack_damage = monster_info['damage']
+        self.resistance = monster_info['resistance']
+        self.attack_radius = monster_info['attack_radius']
+        self.notice_radius = monster_info['notice_radius']
+        self.attack_type = monster_info['attack_type']
+        
     def import_graphics(self, name):
         self.animations = {'idle':[], 'move':[], 'attack':[]}
         main_path = f'./graphics/monsters/{name}/'
         for animation in self.animations.keys():
             self.animations[animation] = import_folder(main_path + animation)
+    
+    def get_player_direction_and_distance(self, player):
+        enemy_vec = pygame.math.Vector2(self.rect.center)
+        player_vec = pygame.math.Vector2(player.rect.center)
+        distance = (player_vec - enemy_vec).magnitude()
+        
+        if distance != 0:
+            direction = (player_vec - enemy_vec).normalize()
+        else:
+            direction = pygame.math.Vector2()
+            
+        return (distance, direction)
+        
+    def get_status(self, player):
+        distance = self.get_player_direction_and_distance(player)[0]
+        
+        if distance <= self.attack_radius:
+            self.status = 'attack'
+        elif distance <= self.notice_radius:
+            self.status = 'move'
+        else:
+            self.status = 'idle'
+    
+    def actions(self, player):
+        if self.status == 'attack':
+            print("attack")
+        elif self.status == 'move':
+            self.direction = self.get_player_direction_and_distance(player)[1]
+            
+        else:
+            self.direction = pygame.math.Vector2()
+    
+    def animate(self):
+        self.frame_index += self.animation_speed   
+                         
+    def update(self):
+        self.move(self.speed)
+    
+    def enemy_update(self,player):
+        self.get_status(player)
+        self.actions(player)
--- level.py
+++ level.py
@@ -89,6 +89,7 @@ class Level:
         #update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+        self.visible_sprites.enemy_update(self.player)
         ## self.messages.display()
         self.ui.display(self.player)
         
@@ -117,3 +118,8 @@ class YsortCameraGroup(pygame.sprite.Group):
         for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft + self.offset
             self.display_surface.blit(sprite.image, offset_pos)
+            
+    def enemy_update(self,player):
+        enemy_sprites = [sprite for sprite in self.sprites() if hasattr(sprite,'sprite_type') and sprite.sprite_type == 'enemy']
+        for enemy in enemy_sprites:
+            enemy.enemy_update(player)
