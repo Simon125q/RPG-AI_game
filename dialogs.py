cf3601bf1d78eea19233aa7fe6cfe441a48a15b9
Simon125q <106029626+Simon125q@users.noreply.github.com>
2023-08-03 15:39:16 +0200

    update

--- dialogs.py
+++ dialogs.py
@@ -19,15 +19,23 @@ class Dialog_box:
         self.bottom_rect = pygame.Rect(self.bottom_pos, (self.width + self.border, self.height + self.border))
         self.top_rect = pygame.Rect(self.top_pos, (self.width, self.height))
         
+        self.font = pygame.font.Font(DIALOG_FONT, DIALOG_FONT_SIZE)
+        self.text_rect = self.text_surf.get_rect(center = self.top_rect.center)
+        
     def display(self, screen, message):
+        words = message.split(' ')
         for msg in range(0, len(message), self.chars_line_length):
-            self.get_action()
+            
+            self.text_surf = self.font.render(text, True, DIALOG_FONT_COLOR)
             self.draw(screen)
+            self.get_action()
+            
             
     def draw(self, screen):
-        pygame.draw.rect(screen, DIALOG_BOX_COLOR, self.rect, border_radius=10)
+        pygame.draw.rect(screen, DIALOG_BOX_COLOR, self.bottom_rect, border_radius=10)
+        pygame.draw.rect(screen, DIALOG_BOX_COLOR, self.top_rect, border_radius=10)
         
-        #screen.blit(self.text_surf, self.text_rect)
+        screen.blit(self.text_surf, self.text_rect)
     
     def get_action(self):
         loop = True
